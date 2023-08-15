4bd2c37fa1b31bdc30164d2c30e63d9eafe905ea
iamcristinanr <crisnieromero@gmail.com>
2023-08-15 18:48:03 +0200

    Perf: Adding CircleViewSet

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,7 +162,11 @@ CELERYD_TASK_SOFT_TIME_LIMIT = 60
 # Django REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES':(
-        'rest_framework.renderers.BrowsableAPIRenderer',
-        'rest_framework.renderers.JSONRenderer'
-    )
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES':(
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3,
 }
