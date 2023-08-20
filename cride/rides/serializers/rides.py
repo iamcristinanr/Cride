5e0d7849ac4452529b79ae72c6df97863f0d300a
iamcristinanr <crisnieromero@gmail.com>
2023-08-20 20:30:10 +0200

    Feat: Join to ride

--- cride/rides/serializers/rides.py
+++ cride/rides/serializers/rides.py
@@ -6,6 +6,10 @@ from rest_framework import serializers
 # Models
 from cride.circles.models import Membership
 from cride.rides.models import Ride
+from cride.users.models import User
+
+#Serializers
+from cride.users.serializers import UserModelSerializer
 
 # Utilities
 from datetime import timedelta
@@ -13,6 +17,10 @@ from django.utils import timezone
 
 class RideModelSerializer(serializers.ModelSerializer):
     """Ride model serializer."""
+    offered_by = UserModelSerializer(read_only=True)
+    offered_in = serializers.StringRelatedField()
+
+    passengers = UserModelSerializer(read_only=True, many=True)
 
     class Meta:
         """Meta class."""
@@ -24,7 +32,13 @@ class RideModelSerializer(serializers.ModelSerializer):
             'offered_by'
             'rating'
         )
-
+    
+    def update(self, instance, data):
+        """Allow updates only before departure date."""
+        now = timezone.now()
+        if instance.departure_date < now:
+            raise serializers.ValidationError('Ongoing rides cannot be modified.')
+        return super(RideModelSerializer, self).update(instance, data)
 
 class CreateRideSerializer(serializers.ModelSerializer):
     """Create ride serializer."""
@@ -93,3 +107,76 @@ class CreateRideSerializer(serializers.ModelSerializer):
         profile.save()
 
         return ride
+    
+class JoinRideSerializer(serializers.ModelSerializer):
+    """Join ride serialier."""
+
+    passenger = serializers.IntegerField()
+    
+    class Meta:
+        """Meta class."""
+
+        model = Ride 
+        fields = ('passenger',)
+    
+    def validate_passenger(self, data):
+        """Verify passenger exits and is a circle member."""
+        try:
+            user = User.objects.get(pk=data)
+        except User.DoesNotExit:
+            raise serialializers.ValidationError('Invalid passenger')
+
+        circle = self.context['circle']
+        try:
+            membership = Membership.objects.get(
+                user=user,
+                circle=circle,
+                is_active=True
+                )
+        except Membership.DoesNotExist:  
+            raise serializers.ValidationError('User is not an active member of the circle.')
+
+        self.context['user'] = user
+        self.context['member'] = membership       
+        return data
+    
+    def validate(self, data):
+        """Verify rides allow new passengers."""
+        offset = timezone.now() + timedelta(minutes=10)
+        ride = self.context['ride']
+        if ride.departure_date <= offset:
+            raise serializers.ValidationError('You cant join this ride now')
+        
+        if ride.available_seats < 1:
+            raise serializers.ValidationError("Ride is already full!")
+        
+        if Ride.objects.filter(passengers__pk=data['passenger']):
+            raise serializers.ValidationError('Passenger is already in this trip')
+
+        return data
+    
+    def update(self, instance, data):
+        """Add passenger to ride, and update stats."""
+        ride = self.context['ride']
+        circle = self.context['circle']
+        user = self.context['user']
+
+        ride.passengers.add(user)
+
+        # Profile
+        profile = user.profile
+        profile.rides_taken += 1
+        profile.save()
+
+        # Membership
+        member = self.context['member']
+        member.rides_taken += 1
+        member.save()
+
+        # Circle
+        circle = self.context['circle']
+        circle.rides_taken += 1
+        circle.save()
+
+        return ride
+
--- cride/rides/views/rides.py
+++ cride/rides/views/rides.py
@@ -1,19 +1,23 @@
 """Rides views."""
 
 # Django REST Framework
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 # Permissions
 from rest_framework.permissions import IsAuthenticated
 from cride.circles.permissions.memberships import IsActiveCircleMember
+from cride.rides.permissions.rides import IsRideOwner
 
 #Filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 # Serializers
 from cride.rides.serializers import (
     CreateRideSerializer,
-    RideModelSerializer
+    RideModelSerializer,
+    JoinRideSerializer
 )
 
 # Models
@@ -26,10 +30,11 @@ from django.utils import timezone
 
 class RideViewSet(mixins.CreateModelMixin,
                 mixins.ListModelMixin,
+                mixins.UpdateModelMixin,
                 viewsets.GenericViewSet):
     """Ride view set."""
 
-    #serializer_class = CreateRideSerializer
+
     permission_classes = [IsAuthenticated, IsActiveCircleMember]
     filter_backends = (SearchFilter, OrderingFilter)
     ordering = ('departure_date', 'arrival_date', 'available_seats')
@@ -42,6 +47,14 @@ class RideViewSet(mixins.CreateModelMixin,
         self.circle = get_object_or_404(Circle, slug_name=slug_name)
         return super(RideViewSet, self).dispatch(request, *args, **kwargs)
 
+    def get_permissions(self):
+        """Assing permission based on action."""
+        permissions = [IsAuthenticated, IsActiveCircleMember]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsRideOwner)
+        return[p() for p in permissions]
+
+
     def get_serializer_context(self):
         """Add circle to serializer context."""
         context = super(RideViewSet, self).get_serializer_context()
@@ -52,6 +65,8 @@ class RideViewSet(mixins.CreateModelMixin,
         """Retrun serializer based on action."""
         if self.action =='create':
             return CreateRideSerializer
+        if self.action == 'update':
+            return JoinRideSerializer
         return RideModelSerializer
     
     def get_queryset(self):
@@ -62,3 +77,19 @@ class RideViewSet(mixins.CreateModelMixin,
             is_active=True,
             available_seats__gte=1
         )
+    
+    @action(detail=True, methods=['post'])
+    def join (self, request, *args, **kwargs):
+        """Add requesting user to ride."""
+        ride = self.get_object()
+        serializer = JoinRideSerializer(
+            ride,
+            data={'passenger': request.user.pk},
+            context={'ride':ride, 'circle':self.circle},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        ride = serializer.save()
+        data = RideModelSerializer(ride).data 
+        return Response(data, status=status.HTTP_200_OK)
+
